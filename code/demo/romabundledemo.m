30817fcc3a02e631e60f473bb155c1f8c4429598
Niclas Borlin <niclas.borlin@cs.umu.se>
2013-10-29 14:09:36 +0000

    Stub for bundle demo.

---
+++ code/demo/romabundledemo.m
@@ -0,0 +1,18 @@
+% Extract name of current directory.
+curDir=fileparts(mfilename('fullpath'));
+
+% Defult to Roma dataset if no data file is specified.
+if ~exist('fName','var')
+    fName=fullfile(curDir,'data','roma.txt');
+    fprintf('No data file specified, using ''%s''.\n',fName);
+    disp(['Set variable ''fName'' to name of Photomodeler Export file if ' ...
+          'you wish to use another file.']);
+    disp(' ')
+end
+    
+fprintf('Loading data file %s...',fName);
+prob=loadpm(fName);
+disp('done.')
+
+s=prob2dbatstruct(prob);
+
