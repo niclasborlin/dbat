7ef7591812985cbbb4534c8ab03190e9443806b1
Niclas Borlin <niclas.borlin@cs.umu.se>
2013-10-30 11:21:45 +0000

    Half-finished bundle.

--- code/dbatSetup.m
+++ code/dbatSetup.m
@@ -7,7 +7,9 @@ baseDir=fileparts(mfilename('fullpath'));
 addpath(fullfile(baseDir,'plotting'),'-end')
 addpath(fullfile(baseDir,'file'),'-end')
 addpath(fullfile(baseDir,'misc'),'-end')
+addpath(fullfile(baseDir,'bundle'),'-end')
 addpath(fullfile(baseDir,'bundle','cammodel'),'-end')
+addpath(fullfile(baseDir,'bundle','lsa'),'-end')
 addpath(fullfile(baseDir,'photogrammetry'),'-end')
 addpath(fullfile(baseDir,'demo'),'-end')
 
