aac7cc89e622c2cfad032b961e820e841ffca5a3
Niclas Borlin <niclas.borlin@cs.umu.se>
2013-11-02 14:20:46 +0000

    Cleaned up bronw_euler_cam.

--- code/bundle/lsa/brown_euler_cam.m
+++ code/bundle/lsa/brown_euler_cam.m
@@ -1,4 +1,4 @@
-function [f,J,JJ]=brown_euler_cam(x,S)
+function [f,J,JJ]=brown_euler_cam(x,s)
 %BROWN_EULER_CAM Residual function for the Brown camera model with Euler angles.
 %
 %   F=BROWN_EULER_CAM(X,S) returns the residual vector F of the camera
@@ -29,50 +29,32 @@ OP=s.OP;
 OP(s.cOP)=x(ixOP);
 
 if (nargout>2)
-    % Numerical approximation of Jacobian (used only for debugging).
-    JJ=jacapprox(mfilename,x,1e-6,{S});
+    % Numerical approximation of Jacobian (for debugging only).
+    JJ=jacapprox(mfilename,x,1e-6,{s});
 end
 
 if (nargout<2)
-	% Project into pinhole camera.
-	xy=pm_multieulerpinhole1(IO,nK,nP,EO,cams,OP,vis);
+    % Only residual vector requested.
+    
+    % Project into pinhole camera.
+    xy=pm_multieulerpinhole1(IO,s.nK,s.nP,EO,s.cams,OP,s.vis);
 
-	% Remove lens distortion from measured points.
-	ptCorr=pm_multilenscorr1(pt,IO,nK,nP,ptCams,size(IO,2));
+    % Remove lens distortion from measured points.
+    ptCorr=pm_multilenscorr1(diag([1,-1])*s.markPts,IO,s.nK,s.nP,s.cams, ...
+                             size(IO,2));
 	
-	f=xy(:)-ptCorr(:);
-    if (any(vIO(:)))
-        f=[f;(x(ixIO(vIO~=0))-oIO(:))./sqrt(vIO(vIO~=0))];
-    end
-    if (any(vEO(:)))
-        f=[f;(x(ixEO(vEO~=0))-oEO(:))./sqrt(vEO(vEO~=0))];
-    end
-    if (any(vOP(:)))
-        f=[f;(x(ixOP(vOP~=0))-oOP(:))./sqrt(vOP(vOP~=0))];
-    end
+    f=xy(:)-ptCorr(:);
 else
-	% Project into pinhole camera.
-	[xy,dIO1,dEO,dOP]=pm_multieulerpinhole1(IO,nK,nP,EO,cams,OP,vis,cIO,cEO,cOP);
+    % Project into pinhole camera.
+    [xy,dIO1,dEO,dOP]=pm_multieulerpinhole1(IO,s.nK,s.nP,EO,s.cams,OP, ...
+                                            s.vis,s.cIO,s.cEO,s.cOP);
 	
-	% Remove lens distortion from measured points.
-	[ptCorr,dIO2]=pm_multilenscorr1(pt,IO,nK,nP,ptCams,size(IO,2),cIO);
+    % Remove lens distortion from measured points.
+    [ptCorr,dIO2]=pm_multilenscorr1(diag([1,-1])*s.markPts,IO,s.nK,s.nP, ...
+                                    s.cams,size(IO,2),s.cIO);
 
-	f=xy(:)-ptCorr(:);
+    f=xy(:)-ptCorr(:);
 	
-    dPI=sparse(length(f),nnz(cPI));
-    dSI=sparse(length(f),nnz(cSI));
-	J=[dIO1-dIO2,dEO,dOP,dPI,dSI];
-    if (any(vIO(:)))
-        f=[f;(x(ixIO(vIO~=0))-oIO(:))./sqrt(vIO(vIO~=0))];
-        J=[J;sparse(1:nnz(vIO),ixIO(vIO~=0),1./sqrt(vIO(vIO~=0)),nnz(vIO),size(J,2))];
-    end
-    if (any(vEO(:)))
-        f=[f;(x(ixEO(vEO~=0))-oEO(:))./sqrt(vEO(vEO~=0))];
-        J=[J;sparse(1:nnz(vEO),ixEO(vEO~=0),1./sqrt(vEO(vEO~=0)),nnz(vEO),size(J,2))];
-    end
-    if (any(vOP(:)))
-        f=[f;(x(ixOP(vOP~=0))-oOP(:))./sqrt(vOP(vOP~=0))];
-        J=[J;sparse(1:nnz(vOP),ixOP(vOP~=0),1./sqrt(vOP(vOP~=0)),nnz(vOP),size(J,2))];
-    end
+    J=[dIO1-dIO2,dEO,dOP];
 end
 
