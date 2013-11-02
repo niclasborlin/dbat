f09211a63a17ac6f5b09aea7c449d8eb7e3978d5
Niclas Borlin <niclas.borlin@cs.umu.se>
2013-11-02 14:12:25 +0000

    White-space cleanup.

--- code/bundle/cammodel/pm_multilenscorr1.m
+++ code/bundle/cammodel/pm_multilenscorr1.m
@@ -32,134 +32,134 @@ dp=[];
 dIO=sparse([],[],[],nPts*2,nnz(cIO));
 
 if (length(cams)==1)
-	% Same camera for all points.
-	cams=repmat(cams,nPts,1);
+    % Same camera for all points.
+    cams=repmat(cams,nPts,1);
 end
 
 if (all(~cIO(:)) & ~(any(cp)))
-	% No partial derivatives.
+    % No partial derivatives.
+    
+    % Preallocate point matrix for speed.
+    xy=zeros(size(p));
+    
+    for i=1:nCams
+        % Get inner orientation.
+        [pp,f,K,P,a,u]=UnpackIO(IO(:,i),nK,nP);
 	
-	% Preallocate point matrix for speed.
-	xy=zeros(size(p));
-
-	for i=1:nCams
-		% Get inner orientation.
-		[pp,f,K,P,a,u]=UnpackIO(IO(:,i),nK,nP);
-	
-		% Get points taken with this camera.
-		ix=cams==i;
-		
-		% Convert to mm.
-		q=diag(u)\p(:,ix);
-		
-		% Lens distortion.
-		lens=pm_lens1(q,pp,K,P);
-		
-		% Correct for lens distortion.
-		xy(:,ix)=q-lens;
-	end
-	xy=xy(:);
+        % Get points taken with this camera.
+        ix=cams==i;
+        
+        % Convert to mm.
+        q=diag(u)\p(:,ix);
+        
+        % Lens distortion.
+        lens=pm_lens1(q,pp,K,P);
+        
+        % Correct for lens distortion.
+        xy(:,ix)=q-lens;
+    end
+    xy=xy(:);
 else
-	% Which partial derivatives are requested?
-	if (length(cIO)==1)
-		cIO=repmat(cIO,size(IO,1),size(IO,2));
-	end
-
-	if (prod(size(cp))==1)
-		cp=repmat(cp,1,nPts);
-	end
+    % Which partial derivatives are requested?
+    if (length(cIO)==1)
+        cIO=repmat(cIO,size(IO,1),size(IO,2));
+    end
+    
+    if (prod(size(cp))==1)
+        cp=repmat(cp,1,nPts);
+    end
+    
+    % Preallocate jacobians.
+    
+    % Number of wanted internal parameters
+    ioCols=nnz(cIO);
+    % Max number of non-zero elements.
+    ioMaxNnz=nPts*2*max(sum(cIO));
+    dIO=sparse([],[],[],nPts*2,ioCols,ioMaxNnz);
+    
+    % Create arrays of columns indices for IO derivatives.
+    [ixpp,ixf,ixK,ixP,ixa,ixu]=CreateIOColumnIndices(cIO,nK,nP);
+    
+    % Number of wanted points.
+    ptCols=2*nnz(cp);
+    ptMaxNnz=4*nPts;
+    dp=sparse([],[],[],nPts*2,ptCols,ptMaxNnz);
+    
+    % Create array of columns indices for point derivatives.
+    ixCol=CreatePtColumnIndices(cp);
+    
+    % Preallocate point matrix for speed.
+    xy=zeros(size(p));
+    
+    for i=1:nCams
+        % Get inner orientation.
+        [pp,f,K,P,a,u]=UnpackIO(IO(:,i),nK,nP);
 	
-	% Preallocate jacobians.
-	
-	% Number of wanted internal parameters
-	ioCols=nnz(cIO);
-	% Max number of non-zero elements.
-	ioMaxNnz=nPts*2*max(sum(cIO));
-	dIO=sparse([],[],[],nPts*2,ioCols,ioMaxNnz);
-
-	% Create arrays of columns indices for IO derivatives.
-	[ixpp,ixf,ixK,ixP,ixa,ixu]=CreateIOColumnIndices(cIO,nK,nP);
-
-	% Number of wanted points.
-	ptCols=2*nnz(cp);
-	ptMaxNnz=4*nPts;
-	dp=sparse([],[],[],nPts*2,ptCols,ptMaxNnz);
-	
-	% Create array of columns indices for point derivatives.
-	ixCol=CreatePtColumnIndices(cp);
-
-	% Preallocate point matrix for speed.
-	xy=zeros(size(p));
-
-	for i=1:nCams
-		% Get inner orientation.
-		[pp,f,K,P,a,u]=UnpackIO(IO(:,i),nK,nP);
-	
-		% Which inner orientation parameters are interesting?
-		[cpp,cf,cK,cP,ca,cu]=UnpackIO(cIO(:,i),nK,nP);
-		
-		% Get points taken with this camera.
-		ix=find(cams==i);
-
-		% Convert to mm.
-		q=diag(u)\p(:,ix);
-		
-		if (any(cp(:)))
-			dqdp=sparse(1:prod(size(q)),1:prod(size(q)),...
+        % Which inner orientation parameters are interesting?
+        [cpp,cf,cK,cP,ca,cu]=UnpackIO(cIO(:,i),nK,nP);
+        
+        % Get points taken with this camera.
+        ix=find(cams==i);
+        
+        % Convert to mm.
+        q=diag(u)\p(:,ix);
+        
+        if (any(cp(:)))
+            dqdp=sparse(1:prod(size(q)),1:prod(size(q)),...
                         repmat(1./u,size(q,2),1),prod(size(q)),prod(size(q)));
-		else
-			dqdp=[];
-		end
-		if (any(cu))
+        else
+            dqdp=[];
+        end
+        if (any(cu))
             dqdu=sparse(1:prod(size(q)),repmat([1;2],1,size(q,2)),...
                         -p(:,ix)./repmat(u.^2,1,nnz(ix)),prod(size(q)),2);
-		else
-			dqdu=[];
-		end
-		
-		% Lens distortion.
-		calcID=any(cu) | any(cp(:));
-		[lens,dldq,dldpp,dldK,dldP]=pm_lens1(q,pp,K,P,calcID,...
-											 any(cpp),any(cK),any(cP));
-		
-		% Correct for lens distortion.
-		xy(:,ix)=q-lens;
-
-		% Calculate row indices in jacobian.
-		ixRow=[(ix(:)-1)*2+1,(ix(:)-1)*2+2]';
-		ixRow=ixRow(:);
-		
-		% IO jacobians.
-		if (any(cpp))
-			dIO(ixRow,ixpp(cpp,i))=-dldpp(:,cpp);
-		end
-		if (cf)
-			% Focal length does not take part in equation.
-			% dIO(ixRow,ixf(i))=0;
-		end
-		if (any(cK))
-			dIO(ixRow,ixK(cK,i))=-dldK(:,cK);
-		end
-		if (any(cP))
-			dIO(ixRow,ixP(cP,i))=-dldP(:,cP);
-		end
-		if (any(ca))
-			% Not implemented yet.
-			% dIO(ixRow,ixa(ca,i))=0
-		end
-		if (any(cu))
-			dIO(ixRow,ixu(cu,i))=dqdu(:,cu)-dldq*dqdu(:,cu);
-		end
-		if (any(cp(ix)))
-			colix=ixCol(:,ix);
-			dqdp=dqdp(:,colix~=0);
-			dp(ixRow,colix(colix~=0))=dqdp-dldq*dqdp;
-		end
-	end
-
-	% Verify
-	%disp([nnz(dIO),ioMaxNnz]);
-	%disp([nnz(dp),ptMaxNnz]);
+        else
+            dqdu=[];
+        end
+        
+        % Lens distortion.
+        calcID=any(cu) | any(cp(:));
+        [lens,dldq,dldpp,dldK,dldP]=pm_lens1(q,pp,K,P,calcID,...
+                                             any(cpp),any(cK),any(cP));
+        
+        % Correct for lens distortion.
+        xy(:,ix)=q-lens;
+        
+        % Calculate row indices in jacobian.
+        ixRow=[(ix(:)-1)*2+1,(ix(:)-1)*2+2]';
+        ixRow=ixRow(:);
+        
+        % IO jacobians.
+        if (any(cpp))
+            dIO(ixRow,ixpp(cpp,i))=-dldpp(:,cpp);
+        end
+        if (cf)
+            % Focal length does not take part in equation.
+            % dIO(ixRow,ixf(i))=0;
+        end
+        if (any(cK))
+            dIO(ixRow,ixK(cK,i))=-dldK(:,cK);
+        end
+        if (any(cP))
+            dIO(ixRow,ixP(cP,i))=-dldP(:,cP);
+        end
+        if (any(ca))
+            % Not implemented yet.
+            % dIO(ixRow,ixa(ca,i))=0
+        end
+        if (any(cu))
+            dIO(ixRow,ixu(cu,i))=dqdu(:,cu)-dldq*dqdu(:,cu);
+        end
+        if (any(cp(ix)))
+            colix=ixCol(:,ix);
+            dqdp=dqdp(:,colix~=0);
+            dp(ixRow,colix(colix~=0))=dqdp-dldq*dqdp;
+        end
+    end
+    
+    % Verify
+    %disp([nnz(dIO),ioMaxNnz]);
+    %disp([nnz(dp),ptMaxNnz]);
 end
 
 
