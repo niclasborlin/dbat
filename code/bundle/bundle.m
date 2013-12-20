ed619f09121ecd8161df00405341aeda3d6296bf
Niclas Borlin <niclas.borlin@cs.umu.se>
2013-12-20 16:53:11 +0000

    First working version of the covariance calculations.

--- code/bundle/bundle.m
+++ code/bundle/bundle.m
@@ -228,7 +228,6 @@ end
 % s0=sqrt(f'*f/(m-n)) in mm, convert to pixels.
 s0=sqrt(f'*f/(length(f)-length(x)))*mean(s.IO(end-1:end));
 
-covMatrices={'cop'};
 % Compute covariance matrices.
 if ~isempty(covMatrices)
     % We may need J'*J many times, precalculate and prefactor.
@@ -410,7 +409,7 @@ if ~isempty(covMatrices)
             
             % Determine block column size such that computed part of inverse is
             % approximately 10M-elements.
-            bsElems=1*1024^2;
+            bsElems=10*1024^2;
             bsCols=floor(bsElems/size(JTJ,1)/max(sum(ix~=0,1)))
             bsCols=min(max(bsCols,1),size(ix,2));
             
@@ -418,13 +417,24 @@ if ~isempty(covMatrices)
             invP=zeros(size(p));
             invP(p)=1:length(p);
             
-            % Sort permuted columns.
-            [dummy,pix]=sort(invP(ix(1,:)));
+            % Sort by original column.
+
+            % Current column (one from each block). Will be zero for
+            % fixed OP.
+            cCol=max(ix,[],1);
+            % Corresponding original column.
+            oCol=zeros(size(cCol));
+            oCol(cCol>0)=invP(cCol(cCol>0));
             
-            % Loop over each OP column.
+            % Get permutation to sort by increasing original column
+            % number. This will move fixed columns to the beginning,
+            % becoming part of the first blocks.
+            [dummy,colPerm]=sort(oCol);
+            
+            % Loop over each OP column in bsCols blocks.
             for j=1:bsCols:size(ix,2)
                 % Columns in block.
-                jCols=pix(j:min(j+bsCols-1,size(ix,2)));
+                jCols=colPerm(j:min(j+bsCols-1,size(ix,2)));
 
                 % Indices into J.
                 jixBlock=ix(:,jCols);
@@ -443,10 +453,11 @@ if ~isempty(covMatrices)
                 C(eix,eix)=Cblock(eix,eix);
                 
                 if (isempty(h) && etime(clock,start)>1) && ...
-                        jCols(end)~=size(ix,2)
+                        j+bsCols-1<size(ix,2)
                     % Only create dialog if execution takes more than 1s
                     % and this iteration is not the last.
-                    h=waitbar(min(j+bsCols-1,size(ix,2))/size(ix,2),'Computing OP covariances');
+                    h=waitbar(min(j+bsCols-1,size(ix,2))/size(ix,2),...
+                              'Computing OP covariances');
                     lapTime=clock;
                 elseif etime(clock,lapTime)>1
                     % Update dialog.
@@ -461,4 +472,93 @@ if ~isempty(covMatrices)
         end
         varargout{i}=C;
     end
+end
+
+
+function C=BlockDiagonalC(R,p,calc,xIx,bsElems,msg)
+%R       - Cholesky factor of the permuted normal matrix A(p,p).
+%p       - Corresponding permutation vector.
+%calc    - logical M-by-N array indicating which data elements have been
+%          estimated and whose covariances should be computed.
+%xIx     - vector of length nnz(calc) with indices of the estimated
+%          elements in the x vector.
+%bsElems - how many elements of the inverse should at most be calculated
+%          at each block iteration? bsElems=1 will use unblocked algorithm.
+%msg     - waitbar message to present. No waitbar is shown if message is empty.
+%C       - M-by-M block-diagonal covariance matrix of size
+%          nnz(calc)-by-nnz(calc).
+
+% Delayed progress dialog.
+start=clock;
+lapTime=start;
+h=[];
+
+% Pre-allocate sparse matrix with place for all diagonal blocks.
+C=spalloc(numel(calc),numel(calc),sum(sum(calc,1).^2));
+
+% Pack indices as the data
+ix=zeros(size(calc));
+ix(calc)=xIx;
+
+% Construct corresponding indexing among the OP parameters.
+ixInt=reshape(1:numel(calc),size(calc));
+
+% Determine block column size such that computed part of inverse is
+% approximately bsElems elements.
+bsCols=floor(bsElems/size(R,1)/max(sum(ix~=0,1)))
+bsCols=min(max(bsCols,1),size(ix,2));
+
+% Create inverse permutation.
+invP=zeros(size(p));
+invP(p)=1:length(p);
+
+% Sort by original column.
+
+% Current column (one from each block). Will be zero for fixed data.
+cCol=max(ix,[],1);
+% Corresponding original column.
+oCol=zeros(size(cCol));
+oCol(cCol>0)=invP(cCol(cCol>0));
+
+% Get permutation to sort by increasing original column number. This will
+% move fixed columns to the beginning, becoming part of the first blocks.
+[dummy,colPerm]=sort(oCol);
+
+% Loop over each column in bsCols blocks.
+for j=1:bsCols:size(ix,2)
+    % Columns in block.
+    jCols=colPerm(j:min(j+bsCols-1,size(ix,2)));
+
+    % Indices into J.
+    jixBlock=ix(:,jCols);
+    jix=jixBlock(calc(:,jCols));
+    % Indices into s.OP
+    eixBlock=ixInt(:,jCols);
+    eix=eixBlock(calc(:,jCols));
+
+    % Compute needed part of inverse and put it into a
+    % temporary matrix.
+    Cblock=spalloc(size(C,1),size(C,1),length(eix)^2);
+    Cblock(eix,eix)=invblock(R,p,jix,'split');
+    % Make it block-diagonal...
+    Cblock=mkblkdiag(Cblock,size(ix,1));
+    % ...and put it into the right part of C.
+    C(eix,eix)=Cblock(eix,eix);
+    
+    if isempty(h) && etime(clock,start)>1 && j+bsCols-1<size(ix,2)
+        % Only create dialog if execution takes more than 1s and this
+        % iteration is not the last.
+        h=waitbar(min(j+bsCols-1,size(ix,2))/size(ix,2),msg);
+        lapTime=clock;
+    elseif etime(clock,lapTime)>1
+        % Update dialog every 1 s.
+        if ishandle(h) % Guard against window close.
+            waitbar(min(j+bsCols-1,size(ix,2))/size(ix,2),h);
+        end
+        lapTime=clock;
+    end
+end
+if ishandle(h), close(h), end
+etime(clock,start)
+
 end
