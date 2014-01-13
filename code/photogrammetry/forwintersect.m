c338d79223a7091b69e2acb7b37d8547d2d0f65c
Niclas Borlin <niclas.borlin@cs.umu.se>
2014-01-13 16:09:11 +0000

    Compute forward intersection for whole project.

--- code/photogrammetry/forwintersect.m
+++ code/photogrammetry/forwintersect.m
@@ -7,10 +7,26 @@ function [s,id,res]=forwintersect(s0,ids)
 %
 %   S=FORWINTERSECT(S0,'all') computes all 3D coordinates.
 %
-%   [S,ID,RES]=... returns the IDs of each computed points in I and the
+%   [S,ID,RES]=... returns the IDs of each computed points in ID and the
 %   corresponding rms object space residual in RES.
 %
 %See also: PM_MULTIFORWINTERSECT.
 
 % $Id$
 
+if strcmp(ids,'all'), ids=s0.OPid; end
+   
+% Remove lens distortion from measured coordinates and convert to mm.
+xy=reshape(pm_multilenscorr1(diag([1,-1])*s0.markPts,s0.IO,s0.nK,s0.nP, ...
+                             s0.ptCams,size(s0.IO,2)),2,[]);
+
+% Extract wanted points.
+i=find(ismember(s0.OPid,ids));
+
+% Compute the forward intersection.
+[OP,res]=pm_multiforwintersect(s0.IO,s0.EO,s0.cams,s0.colPos,xy,i);
+
+% Store result.
+id=s0.OPid(i);
+s=s0;
+s.OP(:,i)=OP;
