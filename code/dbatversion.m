a6ffbc80393f059059b177c0b14a766b38b497e3
Niclas Borlin <niclas.borlin@cs.umu.se>
2014-01-17 07:55:24 +0000

    WIP.

--- code/dbatversion.m
+++ code/dbatversion.m
@@ -1,7 +1,12 @@
-function v=dbatversion
+function [v,date]=dbatversion
 %DBATVERSION Version information for the Damped Bundle Adjustment Toolbox.
 %
-%   DBATVERSION returns a string with the 
+%   DBATVERSION returns a string with the Damped Bundle Adjustment
+%   Toolbox version. The string has the format major.minor.patch.revision.
+%
+%   [V,D]=DBATVERSION also returns a date string D.
+
+% $Id$
 
 % $Author$
 % $Date$
