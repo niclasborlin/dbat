2031c101fa64832e8623765c94ff97662b0c8912
Niclas Borlin <niclas.borlin@cs.umu.se>
2014-01-30 16:58:22 +0000

    Added skipFixed option.

--- code/photogrammetry/forwintersect.m
+++ code/photogrammetry/forwintersect.m
@@ -1,12 +1,16 @@
-function [s,id,res]=forwintersect(s0,ids)
+function [s,id,res]=forwintersect(s0,ids,skipFixed)
 %FORWINTERSECT Perform forward intersection on points in a project.
 %
-%   S=FORWINTERSECT(S0,ID) computes the 3D coordinates of all points in
+%   S=FORWINTERSECT(S0,ID) computes the OP coordinates of all points in
 %   the project S0 with IDs listed in ID using forward intersection.
 %   Points visible in too few images are given NaN coordinates.
 %
-%   S=FORWINTERSECT(S0,'all') computes all 3D coordinates.
+%   S=FORWINTERSECT(S0,'all') computes all OP coordinates.
 %
+%   S=FORWINTERSECT(S0,...,TRUE) does not compute OP coordinates for points
+%   with any fixed coordinate(s), i.e. with a FALSE in the corresponding
+%   S0.cOP entry.
+
 %   [S,ID,RES]=... returns the IDs of each computed points in ID and the
 %   corresponding rms object space residual in RES.
 %
@@ -16,12 +20,20 @@ function [s,id,res]=forwintersect(s0,ids)
 
 if strcmp(ids,'all'), ids=s0.OPid; end
    
+if nargin<3, skipFixed=false; end
+
 % Remove lens distortion from measured coordinates and convert to mm.
 xy=reshape(pm_multilenscorr1(diag([1,-1])*s0.markPts,s0.IO,s0.nK,s0.nP, ...
                              s0.ptCams,size(s0.IO,2)),2,[]);
 
 % Extract wanted points.
-i=find(ismember(s0.OPid,ids));
+if skipFixed
+    isFree=all(s0.cOP,1)';
+else
+    isFree=true;
+end
+    
+i=find(ismember(s0.OPid,ids) & isFree);
 
 % Compute the forward intersection.
 [OP,res]=pm_multiforwintersect(s0.IO,s0.EO,s0.cams,s0.colPos,xy,i);
