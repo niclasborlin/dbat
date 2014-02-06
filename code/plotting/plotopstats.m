05975ee4612eb2a021eacd94d28b50ee07495912
Niclas Borlin <niclas.borlin@cs.umu.se>
2014-02-06 22:50:34 +0000

    Added dynamic tick marks.

--- code/plotting/plotopstats.m
+++ code/plotting/plotopstats.m
@@ -1,4 +1,4 @@
-function hh=plotopstats(s,e,id)
+function [hh,COP]=plotopstats(s,e,varargin)
 %PLOTOPSTATS Plot object point statistics after a bundle.
 %
 %   PLOTOPSTATS(S,E), where S and E are struct given to and returned by
@@ -8,16 +8,29 @@ function hh=plotopstats(s,e,id)
 %   - RMS residual,
 %   - X/Y/Z/total variance.
 %
-%   PLOTOPSTATS(S,E,ID) plots the stats for the OPs with ids in ID only.
+%   PLOTOPSTATS(...,COP) supplies a pre-computed OP covariance matrix COP.
+%
+%   PLOTOPSTATS(...,ID) plots the stats for the OPs with ids in ID only.
 %   ID='all' plots for all OPs.
 %
 %   H=... returns the handle to the plot figure.
+%
+%   [H,COP]=... also returns the computed OP covariance matrix COP.
 %
 %See also: BUNDLE, COVERAGE, PLOTIMAGESTATS.
 
 % $Id$
 
-if nargin<3, id='all'; end
+id='all';
+COP=[];
+
+for i=1:length(varargin)
+    if isvector(varargin{i})
+        id=varargin{i};
+    else
+        COP=varargin{i};
+    end
+end
 
 if strcmp(id,'all'), id=s.OPid; end
 
@@ -33,21 +46,12 @@ clf(fig);
 
 nPts=length(id);
 
-% Where to put tick marks.
-% Repeated.
-ticks=rem(0:nPts-1,floor(sqrt(nPts)))==0;
-% Where IDs jump.
-jump=find(diff(id)~=1);
-%ticks(jump)=true;
-ticks(jump+1)=true;
-ticks=find(ticks);
-
 % Point count plot.
 ax=subplot(4,1,1,'parent',fig);
 n=sum(s.vis(ix,:),2);
 bar(ax,1:nPts,n);
 title(ax,'Point count')
-set(ax,'xtick',ticks,'xgrid','on')
+set(ax,'xtick',[],'xgrid','on')
 set(ax,'xlim',[1,nPts],'ylim',[0,max(n)+1]);
 set(ax,'xticklabel',[])
 axH(1)=ax;
@@ -55,10 +59,10 @@ axH(1)=ax;
 
 % Angles..
 ax=subplot(4,1,2,'parent',fig);
-ang=angles(s)*180/pi;
+ang=angles(s,'Computing angles')*180/pi;
 bar(ax,ix,ang(ix))
 title(ax,'Maximum intersection angles')
-set(ax,'xtick',ticks,'xgrid','on')
+set(ax,'xtick',[],'xgrid','on')
 set(ax,'xlim',[1,nPts]);
 set(ax,'ylim',[0,91])
 set(ax,'xticklabel',[])
@@ -76,14 +80,16 @@ bar(ax,ix,meanOP(ix));
 title(ax,'RMS point residuals (-- = global RMS)')
 line(get(ax,'xlim'),rms*[1,1],'marker','none','linestyle','--',...
      'color',0.5*[0,1,0],'parent',ax);
-set(ax,'xtick',ticks,'xgrid','on')
+set(ax,'xtick',[],'xgrid','on')
 set(ax,'xlim',[1,nPts]);
 set(ax,'xticklabel',[])
 axH(3)=ax;
 
 
 % Variance
-COP=bundle_cov(s,e,'COP');
+if isempty(COP)
+    COP=bundle_cov(s,e,'COP');
+end
 var=reshape(diag(COP),3,[]);
 
 ax=subplot(4,1,4,'parent',fig);
@@ -91,37 +97,80 @@ bar(ax,ix,sqrt([var(1:3,ix);sum(var(1:3,ix),1)]'));
 ylabel(ax,'Project units')
 legend(ax,'X','Y','Z','Total','location','NorthEastOutside')
 title(ax,'OP standard deviations')
-set(ax,'xtick',ticks,'xgrid','on')
+set(ax,'xtick',[],'xgrid','on')
 set(ax,'xlim',[1,nPts])
 set(ax,'xticklabel',' ')
 axH(4)=ax;
 
-pos=ticks;
-labelIds=id(ticks);
-ylim=get(ax,'ylim');
-for i=1:length(pos)
-    text(pos(i),ylim(1)-0.05*diff(ylim),int2str(id(ticks(i))),'parent',ax,...
-         'rotation',90,'interpreter','none','horizontal','right',...
-         'vertical','middle');
-end
-
 xlabel(ax,'OP id')
-
-yLim=nan(length(ax),2);
-for i=1:length(ax)
-    yLim(i,:)=get(ax(i),'ylim');
-end
 
 % Scale plots to have equal width.
 scalewidth(axH);
+
+s=struct('ax',axH,'axL',ax,'id',id);
+
+setticks(s);
 
 % Keep xlim equal during zoom in all plots.
 h=zoom(fig);
 set(h,'actionpostcallback',@postzoom);
 set(h,'motion','horizontal');
-set(fig,'userdata',struct('ax',axH));
+set(fig,'userdata',s);
 
 if nargout>0, hh=fig; end
+
+% Set tick marks such that at most 1 tick per 10 character.
+function setticks(s)
+
+% Axes handles.
+ax=s.ax;
+% Axes handle with id labels.
+axL=s.axL;
+% Number of points
+nPts=length(s.id);
+xLim=get(ax(1),'xlim');
+
+% Get width in characters.
+oldUnits=get(ax(1),'units');
+set(ax(1),'units','characters');
+pos=get(ax(1),'pos');
+set(ax(1),'units',oldUnits);
+w=pos(3);
+
+% Where to put tick marks.
+ix=1:nPts;
+ticks=false(1,nPts);
+% Where IDs jump.
+jump=find(diff(s.id)~=1);
+%ticks(jump)=true;
+ticks(jump+1)=true;
+% Which ticks are in range?
+ticks(ix<xLim(1))=false;
+ticks(ix>xLim(2))=false;
+
+if nnz(ticks)>w/10
+    % Too many, keep only w/10.
+    ticks=find(ticks);
+    ticks=ticks(round(linspace(1,end,w/10)));
+else
+    % Too few, let's add more.
+    nToAdd=floor(w/10)-nnz(ticks);
+    ticks(round(linspace(xLim(1),xLim(2),nToAdd)))=true;
+    ticks=find(ticks);
+end
+
+set(ax,'xtick',ticks);
+
+tag='oplabels';
+delete(findobj(axL,'type','text','tag',tag));
+
+pos=ticks;
+ylim=get(axL,'ylim');
+for i=1:length(pos)
+    text(pos(i),ylim(1)-0.05*diff(ylim),int2str(s.id(ticks(i))),'parent',axL,...
+         'rotation',90,'interpreter','none','horizontal','right',...
+         'vertical','middle','tag',tag);
+end
 
 % Callback function after zooming.
 function postzoom(obj,evd)
@@ -132,4 +181,4 @@ xLim=get(evd.Axes,'xlim');
 fig=get(evd.Axes,'parent');
 s=get(fig,'userdata');
 set(s.ax,'xlim',xLim);
-
+setticks(s);
