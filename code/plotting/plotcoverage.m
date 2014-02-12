a10a51a8b02275bbcb5b42e8d467daceedb8b8b5
Niclas Borlin <niclas.borlin@cs.umu.se>
2014-02-12 14:54:47 +0000

    Added computation of radial coverage.

--- code/plotting/plotcoverage.m
+++ code/plotting/plotcoverage.m
@@ -1,8 +1,10 @@
 function hh=plotcoverage(s,varargin)
 %PLOTCOVERAGE Plot image coverage.
 %
-%   PLOTCOVERAGE(S), where S is a struct returned by PROB2DBATSTRUCT,
-%   plots the image coverage for the project.
+%   PLOTCOVERAGE(S), where S is a struct returned by PROB2DBATSTRUCT, plots
+%   the image coverage for the project. The rectangular, convex hull, and
+%   radial coverage are plotted separately. If the images were taken by
+%   different cameras, the radial coverage may be uninformative.
 %
 %   PLOTCOVERAGE(S,IX) plots the image coverage, convex hull and
 %   rectangular, for the images in IX. IX=='all' includes all images in S.
@@ -17,13 +19,13 @@ function hh=plotcoverage(s,varargin)
 % $Id$
 
 ix='all';
-plotAll=false;
+plotEach=false;
 
 for i=1:length(varargin)
     if isnumeric(varargin{i}) || strcmp(varargin{i},'all')
         ix=varargin{i};
     elseif islogical(varargin{i})
-        plotAll=varargin{i};
+        plotEach=varargin{i};
     else
         error('Bad parameter');
     end
@@ -47,7 +49,7 @@ lgs={};
 lh=[];
 cb=@highlight;
 
-if plotAll
+if plotEach
     for ii=1:length(ix)
         i=ix(ii);
         % Plot observations for this image.
@@ -59,45 +61,93 @@ if plotAll
              'parent',ax,...
              'buttondownfcn',cb);
         % Plot convex hull for this image.
-        [~,~,cHull]=coverage(s,i);
+        [~,~,~,cHull,cl,ch,crp]=coverage(s,i);
         lh(end+1)=line(cHull{1}(1,:),cHull{1}(2,:),'linestyle','-',...
                        'color',cc(rem(ii-1,size(cc,1))+1,:),...
                        'tag',sprintf('chull%d',i),'userdata',i, ...
                        'parent',ax,...
                        'buttondownfcn',cb);
         lgs{end+1}=sprintf('Image %d',i);
+        % Plot rectangular hull for this image.
+        clh=[cl,ch];
+        line(clh(1,[1,2,2,1,1]),clh(2,[1,1,2,2,1]),'linestyle','-.',...
+             'color',cc(rem(ii-1,size(cc,1))+1,:),...
+             'tag',sprintf('recthull%d',i),'userdata',i, ...
+             'parent',ax,...
+             'buttondownfcn',cb);
+        % Plot radial hull for this image.
+        % Radial line
+        line(crp(1,:),crp(2,:),'linestyle','--','marker','o',...
+             'color',cc(rem(ii-1,size(cc,1))+1,:),...
+             'tag',sprintf('radhull%d',i),'userdata',i, ...
+             'parent',ax,...
+             'buttondownfcn',cb);
+        % Circle
+        c=crp(:,1);
+        r=norm(diff(crp,[],2));
+        t=linspace(0,2*pi);
+        cpt=repmat(c,1,length(t))+r*[cos(t);sin(t)];
+        line(cpt(1,:),cpt(2,:),'linestyle','--',...
+             'color',cc(rem(ii-1,size(cc,1))+1,:),...
+             'tag',sprintf('radhull%d',i),'userdata',i, ...
+             'parent',ax,...
+             'buttondownfcn',cb);
     end
-    ii=length(ix+1);
+    ii=length(ix)+1;
 else
     ii=1;
+    % Plot observations for all wanted images.
+    j=s.colPos(:,ix);
+    j=j(j~=0);
     % Plot all points.
-    line(s.markPts(1,:),s.markPts(2,:),'linestyle','none','marker','x',...
+    line(s.markPts(1,j),s.markPts(2,j),'linestyle','none','marker','x',...
          'color',cc(rem(ii-1,size(cc,1))+1,:),...
          'tag','obsall','userdata',0,...
          'parent',ax,...
          'buttondownfcn',cb);
+    ii=ii+1;
 end
 
 % Coverage for all points.
-[c,cr,cHull,cl,ch]=coverage(s,ix,true);
+[c,cr,crr,cHull,cl,ch,crp]=coverage(s,ix,true);
 
 % Rectangular hull for all images.
 clh=[cl,ch];
-lh(end+1)=line(clh(1,[1,2,2,1,1]),clh(2,[1,1,2,2,1]),'linestyle','--',...
+lh(end+1)=line(clh(1,[1,2,2,1,1]),clh(2,[1,1,2,2,1]),'linestyle','-.',...
                'color',cc(rem(ii-1,size(cc,1))+1,:),...
                'tag','rectall','userdata',-1,...
                'parent',ax,...
                'buttondownfcn',cb);
 lgs{end+1}='Rect hull';
-ii=ii+1;
+%ii=ii+1;
 
-% Conver hull for all images.
+% Convex hull for all images.
 lh(end+1)=line(cHull{1}(1,:),cHull{1}(2,:),'linestyle','-',...
                'color',cc(rem(ii-1,size(cc,1))+1,:),...
-               'tag','rectall','userdata',-2,...
+               'tag','rectall','userdata',-1,...
                'parent',ax,...
                'buttondownfcn',cb);
 lgs{end+1}='Convex hull';
+
+% Plot radial hull for this image.
+
+% Radial line
+lh(end+1)=line(crp(1,:),crp(2,:),'linestyle','--','marker','o',...
+               'color',cc(rem(ii-1,size(cc,1))+1,:),...
+               'tag','radall','userdata',-1,...
+               'parent',ax,...
+               'buttondownfcn',cb);
+lgs{end+1}='Radial hull';
+% Circle
+c0=crp(:,1);
+r=norm(diff(crp,[],2));
+t=linspace(0,2*pi);
+cpt=repmat(c0,1,length(t))+r*[cos(t);sin(t)];
+line(cpt(1,:),cpt(2,:),'linestyle','--',...
+     'color',cc(rem(ii-1,size(cc,1))+1,:),...
+     'tag','radall','userdata',-1,...
+     'parent',ax,...
+     'buttondownfcn',cb);
 ii=ii+1;
 
 [legh,objh,outh,outm]=legend(ax,lh,lgs{:},'Location','NorthEastOutside');
@@ -114,14 +164,14 @@ for i=1:length(lh)
 end
 
 if length(ix)==size(s.EO,2)
-    title(ax,'Image coverage for all images');
+    title(ax,'Coverage for all images');
 else
     ss=sprintf('%d,',ix);
     ss(end)=[];
-    title(ax,['Image coverage for images ',ss]);
+    title(ax,['Coverage for images ',ss]);
 end
-xlabel(ax,sprintf('Rectangular coverage=%d%%. Convex hull coverage=%d%%.',...
-                  round(c*100),round(cr*100)));
+xlabel(ax,sprintf('Rectangular/convex hull/radial coverage = %d%% / %d%% / %d%%.',...
+                  round(cr*100),round(c*100),round(crr*100)));
 
 if nargout>0, hh=h; end
 
