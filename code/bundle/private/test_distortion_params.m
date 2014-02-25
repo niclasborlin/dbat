13ea60333f677a9addd6c9a543cc2c898de80444
Niclas Borlin <niclas.borlin@cs.umu.se>
2014-02-25 11:18:07 +0000

    Added computation of p values for lens distortion coefficients.

---
+++ code/bundle/private/test_distortion_params.m
@@ -0,0 +1,38 @@
+function [K,P]=test_distortion_params(s,e)
+%TEST_DISTORTION_PARAMS Chi-square test of lens distortion parameters.
+%
+%   [K,P]=test_distortion_params(S,E) computes p values from the cumulative
+%   Chi-square distribution function for the lens distortion parameters. The
+%   structures S and E are given to and returned by BUNDLE, respectively.
+%   The vector K returns the p values for each radial distortion koefficient
+%   K1, K2, K3, treated separately (DOF=1). The scalar P returns the p value
+%   for the P1, P2 parameters together (DOF=2). NaN values are returned
+%   for unestimated parameters.
+%
+%See also: CUMCHI2, HIGH_IO_CORRELATIONS.
+
+% $Id$
+
+% Estimated IO values and their covariances.
+x=s.IO;
+CIO=bundle_cov(s,e,'CIO');
+
+% Test radial coefficients individually.
+K=nan(3,1);
+for i=1:length(K)
+    % Chi-square statistic is (x-mu)'*inv(C)*(x-mu), where x is N(mu,C).
+    ix=3+i;
+    if s.cIO(ix)
+        v=(x(ix)-0)^2/CIO(ix,ix);
+        K(i)=cumchi2(v,1);
+    end
+end
+
+% Test tangential coefficients together.
+ix=7:8;
+if all(s.cIO(ix))
+    v=x(ix)'*(CIO(ix,ix)\x(ix));
+    P=cumchi2(v,2);
+else
+    P=nan;
+end
