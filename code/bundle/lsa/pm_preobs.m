45301642eb796c65ebdafee6f53b4c89fb0ad86d
Niclas Borlin <niclas.borlin@cs.umu.se>
2015-11-05 20:09:46 +0000

    Modified code to try to use prior OP, IO obs.

---
+++ code/bundle/lsa/pm_preobs.m
@@ -0,0 +1,52 @@
+function [f,J,JJ]=pm_preobs(x,s)
+%PM_PREOBS Prior observation residual and Jacobian for bundle adjustment.
+%
+%   F=PM_PREOBS(X,S) returns the residual vector F for the prior
+%   observations for the camera network defined in S compared to the
+%   approximate values in X. The prior observation residual
+%   F=[fIO;fEO;fOP] consists of IO, EO, and OP residuals, in that
+%   order.
+%
+%   [F,J]=... also returns the analytical Jacobian J.
+%
+%   [F,J,JJ]=... furthermore returns the numerical Jacobian J.
+
+% $Id$
+
+% Create index vectors for unknown parameters.
+[ixIO,ixEO,ixOP,n]=indvec([nnz(s.estIO),nnz(s.estEO),nnz(s.estOP)]);
+
+% Copy the current approximations of the unknown values.
+IO=s.IO;
+IO(s.estIO)=x(ixIO);
+EO=s.EO;
+EO(s.estEO)=x(ixEO);
+OP=s.OP;
+OP(s.estOP)=x(ixOP);
+
+if (nargout>2)
+    % Numerical approximation of Jacobian (for debugging only).
+    JJ=jacapprox(mfilename,x,1e-6,{s});
+end
+
+if (nargout<2)
+    % Only residual vector requested.
+    
+    fIO=IO(s.useIOobs)-s.IOobs(s.useIOobs);
+    fEO=EO(s.useEOobs)-s.EOobs(s.useEOobs);
+    fOP=OP(s.useOPobs)-s.OPobs(s.useOPobs);
+    
+    f=[fIO(:);fEO(:);fOP(:)];
+else
+    fIO=IO(s.useIOobs)-s.IOobs(s.useIOobs);
+    JIO=sparse(1:length(fIO),ixIO(s.useIOobs),1,length(fIO),length(x));
+    fEO=EO(s.useEOobs)-s.EOobs(s.useEOobs);
+    JEO=sparse(1:length(fEO),ixEO(s.useEOobs),1,length(fEO),length(x));
+    fOP=OP(s.useOPobs)-s.OPobs(s.useOPobs);
+    JOP=sparse(1:length(fOP),ixOP(s.useOPobs),1,length(fOP),length(x));
+
+    f=[fIO(:);fEO(:);fOP(:)];
+    J=[JIO;JEO;JOP];
+end
+
+
