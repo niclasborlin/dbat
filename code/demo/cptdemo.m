43f3fd2f97d7dd1ebde974649a503bf6cbea0d74
Niclas Borlin <niclas.borlin@cs.umu.se>
2016-04-02 22:02:41 +0200

    WIP.

--- code/demo/cptdemo.m
+++ code/demo/cptdemo.m
@@ -6,7 +6,7 @@ dampings={'none','gna','lm','lmp'};
 dampings=dampings(2);
 
 if ~exist('fName','var')
-    stub='weighted1-5';
+    stub='w15-op1';
     dataDir=stub; %'fixed1-5';
     fName=fullfile(curDir,'data','weighted','sxb',dataDir,...
                    [stub,'-pmexport.txt']);
