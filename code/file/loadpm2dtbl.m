ddefed0b43349e4d06f7c75320c9afc92e80cb19
Niclas Borlin <niclas.borlin@cs.umu.se>
2016-04-03 16:48:24 +0200

    Added functions to handle Photomodeler + Photoscan 2D marker data.

---
+++ code/file/loadpm2dtbl.m
@@ -0,0 +1,66 @@
+function pts=loadpm2dtbl(fName)
+%LOADPM2DTBL Load 2D point table exported from Photomodeler.
+%
+%   PTS=LOADPM3DTBL(FNAME) loads 3D point data from the exported 3D
+%   point table in the file FNAME. The data is returned in a struct
+%   PTS with fields
+%       id   - N-array with id numbers.
+%       imNo - N-array with image numbers.
+%       pos  - 2-by-N array of measured positions.
+%       res  - 2-by-N array of residuals.
+% 
+%       N is the number of loaded points.
+%
+%   The function assumes the file is a save of a Photomodeler point
+%   table or a SMART point table. Only lines starting with a
+%   numeric ID are processed.
+
+% $Id$
+
+[fid,msg]=fopen(fName,'rt');
+if fid<0
+    error('Failed to open %s for reading: %s.\n',fName,msg); ...
+end
+
+digits='0':'9';
+
+id=zeros(1,0);
+imNo=zeros(1,0);
+pos=zeros(2,0);
+res=zeros(2,0);
+while ~feof(fid)
+    % Read one line.
+    s=strtrim(fgetl(fid));
+    
+    % Skip unless first non-white-space is a digit.
+    if isempty(s) || ~any(s(1)==digits)
+        continue
+    end
+
+    [ii,mm,pp,rr]=ParsePointLine(s);
+    
+    id(end+1)=ii;
+    imNo(end+1)=mm;
+    pos(:,end+1)=pp;
+    res(:,end+1)=rr;
+end
+
+fclose(fid);
+
+pts=struct('id',id,'imNo',imNo,'pos',pos,'res',res);
+
+
+function [id,imNo,pos,res]=ParsePointLine(s)
+% Parse line from 2D point table.
+
+% Id, imNo, x, y, resX, resY
+v=sscanf(s,'%d,%d,%g,%g,%g,%g');
+
+if length(v)~=6
+    error('Unable to parse string: %s',s);
+end
+
+id=v(1);
+imNo=v(2);
+pos=v(3:4);
+res=v(5:6);
---
+++ code/file/writepsmarkertbl.m
@@ -0,0 +1,40 @@
+function [ok,msg]=writepsmarkertbl(psTbl,fName)
+%WRITEPSMARKERTBL Write Photoscan marker table to a text file.
+%
+%    [OK,MSG]=WRITEPSMARKERTBL(PSTBL,FNAME) writes a Photoscan marker
+%    table PSTBL to the text file FNAME, corresponding to the
+%    <markers> section of the doc.xml file within a Photoscan
+%    archive. If the file name is omitted, the text is written to
+%    stdout instead. On failure a return status and message is returned.
+
+% $Id$
+
+msg='';
+
+if nargin<2
+    fid=1;
+else
+    [fid,msg]=fopen(fName,'wt');
+    if fid<0
+        ok=false;
+        return;
+    end
+end
+
+ft={'false','true'};
+
+fprintf(fid,'          <markers>\n');
+ids=unique(psTbl.markerId);
+for i=1:length(ids)
+    fprintf(fid,'            <marker marker_id="%d">\n',ids(i));
+    for j=find(psTbl.markerId==ids(i))'
+        fprintf('              <location camera_id="%d" pinned="%s" x="%.10f" y="%.10f"/>\n',...
+                psTbl.cameraId(j),ft{psTbl.pinned(j)+1},psTbl.pos(:,j));
+    end
+    fprintf(fid,'            </marker>\n');
+end
+fprintf(fid,'          </markers>\n');
+
+if fid>2
+    fclose(fid);
+end
