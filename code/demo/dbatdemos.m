6eb81131627d53f0d5c08d24f578348a54a88cef
Niclas Borlin <niclas.borlin@cs.umu.se>
2016-06-14 08:33:33 +0200

    Added initial list of demos and their data.

---
+++ code/demo/dbatdemos.m
@@ -0,0 +1,13 @@
+%DBATDEMOS List of DBAT demos.
+%
+%
+%
+%
+%
+% Prague 2016:
+%    Call:
+%        PRAGUE2016_CAM(L), where L is 'C1' or 'C2'.
+%    Data files:
+%        DBATHOME/code/demos/data/prague2016/cam
+%
+%
