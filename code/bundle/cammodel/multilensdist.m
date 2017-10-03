4f9936a8b7f1359aab2758916f1d1ebf5af4144c
Niclas Borlin <niclas.borlin@cs.umu.se>
2017-10-03 10:29:09 +0200

    Cleaned up multidist, added selftest.

--- code/bundle/cammodel/multilensdist.m
+++ code/bundle/cammodel/multilensdist.m
@@ -1,48 +1,62 @@
-function [ld,dIO,dp]=pm_multilens1(p,IO,nK,nP,cams,nCams,cIO,cp)
-%PM_MULTILENS1 Compute lens distortion in multiple images.
+function [ld,dIO,dp]=multilensdist(p,IO,nK,nP,cams,cIO,cp)
+%MULTILENSDIST Compute lens distortion in multiple images.
 %
-%[ld,dIO,dp]=pm_multilens1(p,IO,nK,nP,cams,nCams[,cIO,cp])
-%p      - 2 x n array with image points in mm with negative y coordinates.
-%IO     - matrix with camera inner orientation as columns
-%         pp - principal point [xp;yp].
-%         f  - focal length.
-%         K  - radial lens distortion coefficients.
-%         P  - tangential lens distortion coefficients.
-%         a  - with affine lens distortion coefficients.
-%         u  - image units in x and y direction.
-%nK     - number of radial koefficients.
-%nP     - number of tangential koefficients.
-%cams   - vector of camera numbers for each point.
-%nCams  - total number of cameras.
-%cIO    - should we calculate partial derivatives w.r.t. internal
-%         orientation? Scalar or matrix of same size as IO.
-%cp     - should we calculate partial derivatives w.r.t. points?
-%         2 x n or 1 x n vector.
-%ld     - 2 x n array with lens distortion for each point in p.
-%dIO    - jacobian w.r.t. internal orientation.
-%dp     - jacobian w.r.t. 2d points.
+%   MULTILENSDIST(P,IO,nK,nP) computes the lens distortion of the
+%   points in the 2-by-N array P using the internal orientation
+%   parameters in the 16-vector IO. nK and nP contain the radial and
+%   tangential distortion parameters, respectively, stored in IO.
+%
+%   MULTILENSDIST(P,IO,nK,nP,CAMS), where IO is 16-by-nCams, and
+%   CAMS is as N-vector, computes the lens distortion for points in
+%   multiple cameras. The vector CAMS indicate which IO column
+%   holds the parameters for the camera the imaged each point.
+%
+%   [LD,dIO,dP]=MULTILENSDIST(...) also computes the Jacobians dIO and
+%   dP with respect to the IO parameters, and the points P,
+%   respectively. If a subset of the Jacobians is wanted, use
+%   [LD,dIO,dP]=MULTILENSDIST(P,IO,nK,nP,CAMS,cIO,cP), where cIO
+%   and cP indicate what Jacobians are requested. cIO can be scalar
+%   or of the same size as IO. cP must be scalar.
+%
+%   Each column of the IO array holds the following parameters:
+%       pp - principal point (2-vector),
+%       f  - focal length (scalar),
+%       K  - radial lens distortion coefficients (nK-vector),
+%       P  - tangential lens distortion coefficients (nP-vector),
+%       b  - aspect and skew parameters (2-vector),
+%       s  - sensor width and height (2-vector),
+%       i  - image width and height (2-vector),
+%       r  - image resolution (2-vector)
+%
+%   See also: BROWNDIST.
 
-if nargin<7, cIO=(nargout>1); end
-if nargin<8, cp=(nargout>2); end
+if nargin==1 && ischar(p), selftest, return, end
 
-% Total number of points.
-nPts=size(p,2);
+if nargin<5, cams=1; end
+if nargin<6, cIO=(nargout>1); end
+if nargin<7, cp=(nargout>2); end
 
-dp=[];
-dIO=sparse([],[],[],nPts*2,nnz(cIO));
-
-if length(cams)==1
-    % Same camera for all points.
-    cams=repmat(cams,nPts,1);
+if ~(isscalar(cIO) || all(size(cIO)==size(IO)))
+    error('%s: Bad size of cIO parameter',mfilename);
 end
 
-if all(~cIO(:)) && ~(any(cp))
+if ~isscalar(cp), error('%s: cp parameter must be scalar',mfilename); end
+    
+% Total number of points.
+n=size(p,2);
+
+if isscalar(cams)
+    % Same camera for all points.
+    cams=repmat(cams,n,1);
+end
+
+if ~(any(cIO(:)) || cp)
     % No partial derivatives.
     
     % Preallocate correction matrix for speed.
     ld=nan(size(p));
     
-    for i=1:nCams
+    for i=1:max(cams)
         % Get inner orientation.
         [pp,~,K,P]=unpackio(IO(:,i),nK,nP);
 	
@@ -57,37 +71,31 @@ if all(~cIO(:)) && ~(any(cp))
         ld(:,ix)=lens;
     end
 else
-    % Which partial derivatives are requested?
+    % Which IO partial derivatives are requested?
     if isscalar(cIO)
-        cIO=repmat(cIO,size(IO,1),size(IO,2));
+        cIO=repmat(cIO,size(IO));
     end
     
-    if isscalar(cp)
-        cp=repmat(cp,1,nPts);
-    end
-    
-    % Preallocate jacobians.
-    
-    % Number of wanted internal parameters
-    ioCols=nnz(cIO);
-    % Pre-allocate jacobian.
-    dIO=zeros(2*nPts,ioCols);
+    % Pre-allocate dense Jacobian.
+    dIO=zeros(2*n,nnz(cIO));
     
     % Create arrays of columns indices for IO derivatives.
-    [ixpp,ixf,ixK,ixP,ixa,ixu]=createiocolumnindices(cIO,nK,nP); %#ok<ASGLU>
-    
-    % Number of wanted points.
-    ptCols=2*nnz(cp);
-    ptMaxNnz=4*nPts;
-    dp=sparse([],[],[],nPts*2,ptCols,ptMaxNnz);
-    
-    % Create array of columns indices for point derivatives.
-    ixCol=createptcolumnindices(cp);
+    [ixpp,~,ixK,ixP]=createiocolumnindices(cIO,nK,nP);
+
+    if cp
+        % Jacobian w.r.t. points will be block diagonal with 2-by-2
+        % blocks. Preallocate array to hold blocks.
+        dpBlocks=zeros(2,2,n);
+        % Ask browndist for packed Jacobian.
+        cp=-1;
+    else
+        dp=sparse(2*n,2*n);
+    end
     
     % Preallocate correction matrix for speed.
     ld=nan(size(p));
     
-    for i=1:nCams
+    for i=1:max(cams)
         % Get inner orientation.
         [pp,~,K,P]=unpackio(IO(:,i),nK,nP);
 	
@@ -104,25 +112,58 @@ else
         % Correct for lens distortion.
         ld(:,ix)=lens;
         
-        % Calculate row indices in jacobian.
-        ixRow=[(ix(:)-1)*2+1,(ix(:)-1)*2+2]';
-        ixRow=ixRow(:);
+        % Calculate row indices in Jacobians.
+        ixRow=[(ix-1)*2+1;(ix-1)*2+2];
         
         % IO jacobians.
         if any(cpp)
-            dIO(ixRow,ixpp(cpp,i))=-dldpp(:,cpp);
+            dIO(ixRow,ixpp(cpp,i))=dldpp(:,cpp);
         end
         if any(cK)
-            dIO(ixRow,ixK(cK,i))=-dldK(:,cK);
+            dIO(ixRow,ixK(cK,i))=dldK(:,cK);
         end
         if any(cP)
-            dIO(ixRow,ixP(cP,i))=-dldP(:,cP);
+            dIO(ixRow,ixP(cP,i))=dldP(:,cP);
         end
-        if any(cp(ix))
-            warning('Not verified'); % TODO: Verify this.
-            colix=ixCol(:,ix);
-            dqdp=dqdp(:,colix~=0);
-            dp(ixRow,colix(colix~=0))=dqdp-dldq*dqdp;
+        if cp && nnz(ix)>0
+            % Put diagonal blocks in stack.
+            dpBlock(:,:,ix)=dldq;
         end
     end
+end
+
+if cp
+    % Convert the diagonal blocks to block-diagonal matrix.
+    [i,j,v]=find(reshape(dpBlock,2,2*n));
+    dp=sparse(i+floor((j-1)/2)*2,j,v,2*n,2*n);
+end
+
+function selftest
+% Compare the analytical and numerical Jacobians and report the
+% maximum deviation. Should be below 1e-9.
+
+vec=@(x)x(:);
+
+nCams=35;
+IO=rand(16,nCams);
+nK=3;
+nP=2;
+n=15;
+p=rand(2,n);
+cams=floor(rand(1,n)*nCams)+1;
+fIO=@(IO)vec(multilensdist(p,reshape(IO,16,[]),nK,nP,cams));
+fp=@(p)vec(multilensdist(reshape(p,2,[]),IO,nK,nP,cams));
+dIO2=jacapprox(fIO,IO(:));
+dp2=jacapprox(fp,p(:));
+[~,dIO,dp]=multilensdist(reshape(p,2,[]),IO,nK,nP,cams);
+mx=-inf;
+mx=max(mx,full(max(max(abs(dIO-dIO2)))));
+mx=max(mx,full(max(max(abs(dp-dp2)))));
+thres=1e-9;
+if mx<thres
+    fprintf('%s selftest: Maximum diff = %g, max expected=%g, OK.\n',mfilename,mx,...
+            thres);
+else
+    warning('%s selftest: Maximum diff = %g, max expected=%g.\n',mfilename,mx,...
+            thres);
 end
