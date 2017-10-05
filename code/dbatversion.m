532a3b573943a770d7d61131a6cd9e56b55cc163
Niclas Borlin <niclas.borlin@cs.umu.se>
2017-10-05 22:23:45 +0200

    Bumped version number.

--- code/dbatversion.m
+++ code/dbatversion.m
@@ -12,9 +12,9 @@ function [v,date]=dbatversion(gitToo)
 if nargin<1, gitToo=false; end
 
 % Should always be x.y.z or x.y.z.w.
-v='0.6.4.0';
+v='0.6.4.1';
 
-date='2017-10-04';
+date='2017-10-05';
 
 if gitToo
     g=gitversion;
