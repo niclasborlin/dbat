3d8c9cbe2027b603d5b248896fadf4f8914419f1
Niclas Borlin <niclas.borlin@cs.umu.se>
2017-10-11 15:12:45 +0200

    Added return of the full transformation matrix.

--- code/misc/rigidalign.m
+++ code/misc/rigidalign.m
@@ -1,14 +1,20 @@
-function [R,d]=rigidalign(X,Y)
+function [T,R,d]=rigidalign(X,Y)
 %RIGIDALIGN Compute rigid-body transformation between two point sets.
 %
-%   [R,d]=RIGIDALIGN(X,Y), where X and Y are M-by-N arrays of M-D
+%   [T,R,d]=RIGIDALIGN(X,Y), where X and Y are M-by-N arrays of M-D
 %   points, computes the rigid-body transformation that minimizes
 %
 %      sum  norm( R * X(:,i) + d - Y(:,i) )^2,
 %       i
 %
 %   where R is an M-by-M rotation matrix and d is an M-by-1
-%   translation vector.
+%   translation vector. The M+1-by-M+1 matrix T is the homogeneous
+%   transformation matrix
+%
+%       T=[ R  d
+%           0  1 ]
+%
+%   that transforms X to Y.
 %
 %   References: Soderkvist and Wedin, (1993), Determining the
 %   movement of the skeleton using well-configured markers. Journal
@@ -32,10 +38,12 @@ B=Y-repmat(ym,1,n);
 C=B*A';
 
 % Perform singular value decomposition of C.
-[P,G,Q]=svd(C);
+[P,~,Q]=svd(C);
 
 % Compute optimal rotation matrix.
 R=P*diag([ones(1,m-1),det(P*Q')])*Q';
 
 % Compute optimal shift.
 d=ym-R*xm;
+
+T=[R,d;zeros(1,m),1];
