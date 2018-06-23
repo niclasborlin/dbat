281dec8386423757b2b03674d63be1b71d614e46
Niclas Borlin <niclas.borlin@cs.umu.se>
2018-06-23 15:32:25 +0200

    Bugfix to handle multiple cameras.

--- code/bundle/cammodel/private/unpackio.m
+++ code/bundle/cammodel/private/unpackio.m
@@ -2,9 +2,9 @@ function [pp,f,K,P,b,u]=unpackio(IO,nK,nP)
 %Unpack inner orientation vector.
 
 
-pp=IO(1:2);
-f=IO(3);
-K=IO(3+(1:nK));
-P=IO(3+nK+(1:nP));
-b=IO(3+nK+nP+(1:2));
-u=IO(3+nK+nP+6+(1:2));
+pp=IO(1:2,:);
+f=IO(3,:);
+K=IO(3+(1:nK),:);
+P=IO(3+nK+(1:nP),:);
+b=IO(3+nK+nP+(1:2),:);
+u=IO(3+nK+nP+6+(1:2),:);
