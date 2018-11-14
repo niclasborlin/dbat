5df4d035ba326bd49906836bb9f231d0edc80165
Niclas Borlin <niclas.borlin@cs.umu.se>
2018-11-14 09:18:24 +0100

    Stub.

---
+++ code/misc/matchcpt.m
@@ -0,0 +1,8 @@
+function s=matchcpt(s,pts)
+%MATCHCPT Match loaded control point table with DBAT struct.
+%
+%   S=MATCHCPT(S,PTS) matches the control point table PTS with the
+%   information in the DBAT structure S. The OP information in
+%   S.prior is updated on return by the matched points.
+
+
