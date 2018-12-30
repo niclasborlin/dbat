90f533d8d95278ccfacbc4f12e361cd04b28d594
Niclas Borlin <niclas.borlin@cs.umu.se>
2018-12-30 10:34:01 +0100

    Added match selection parameter.

--- code/misc/matchcpt.m
+++ code/misc/matchcpt.m
@@ -1,23 +1,40 @@
-function [i,j]=matchcpt(s,pts)
+function [i,j]=matchcpt(s,pts,match)
 %MATCHCPT Loaded control point table with DBAT struct.
 %
-%   [I,J]=MATCHCPT(S,PTS) matches the control point table PTS with the
-%   information in the DBAT structure S. Points can be matched
-%   using id and/or label, depending on what information is
-%   available in PTS. If points are matched by id, S.OP.rawId(I)
-%   corresponds to PTS.id(J). If points are matched by label,
-%   S.OP.label(I) corresponds to PTS.name(J).
-%
-%   Points are matched by id if pts.id are non-NaN. Points are matched
-%   by label if pts.name are non-empty. If both conditions are true,
-%   points are matched using both criteria. An error is issued if the
-%   matchings are inconsistent.
+%   [I,J]=MATCHCPT(S,PTS,MATCH) matches the control point table PTS with the
+%   information in the DBAT structure S. Points can be matched using id and/or
+%   label, depending on the MATCH parameter. If MATCH=='id', S.OP.rawId(I)
+%   corresponds to PTS.id(J). If MATCH=='label', S.OP.label(I) corresponds to
+%   PTS.name(J). If MATCH=='both', both will be true. If MATCH=='auto'
+%   (default), points are matched by id if pts.id are non-NaN. Points are
+%   matched by label if pts.name are non-empty. If both conditions are true,
+%   points are matched using both criteria. An error is issued if the matchings
+%   are inconsistent.
 %
 %See also: LOADCPT, SETCPT.
 
+if nargin<3
+    match='auto';
+end
+
 % How should we match?
-matchById=any(~isnan(pts.id));
-matchByLabel=any(~cellfun(@isempty,pts.name));
+matchById=false;
+matchByLabel=false;
+
+switch match
+  case 'id'
+    matchById=true;
+  case 'label'
+    matchByLabel=true;
+  case 'both'
+    matchById=true;
+    matchByLabel=true;
+  case 'auto'
+    matchById=any(~isnan(pts.id));
+    matchByLabel=any(~cellfun(@isempty,pts.name));
+  otherwise
+    error('MATCHCPT: Bad match string');
+end
 
 il=[]; jl=[]; ii=[]; ji=[];
 
