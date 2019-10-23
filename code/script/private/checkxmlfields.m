14f977bfb06448137987b6274c322613d18fa575
Niclas Borlin <niclas.borlin@cs.umu.se>
2019-10-23 15:57:08 +0200

    Simplified use of checkxmlfields

--- code/script/private/checkxmlfields.m
+++ code/script/private/checkxmlfields.m
@@ -5,14 +5,15 @@ function [ok,msg]=checkxmlfields(s,fields,mandatory)
 %   only fields listed in the N-cell string array FIELDS. To test for
 %   a single field, FIELDS may be a char array. All FIELDS are assumed
 %   to be mandatory. Use CHECKXMLFIELDS(S,FIELDS,MAN), where MAN is a
-%   logical N-vector. The function returns OK as TRUE if all mandatory
-%   fields are present and no unknown field.
+%   scalar or logical N-vector. The function returns OK as TRUE if all
+%   mandatory fields are present and no unknown field.
 %
 %   [OK,MSG]=... also returns an error message, indicating the
 %   offending field name(s).
 
 if ischar(fields), fields={fields}; end
 if nargin<3, mandatory=true(size(fields)); end
+if isscalar(mandatory), mandatory=repmat(mandatory,size(fields)); end
 
 msg='';
 
