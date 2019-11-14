c64c5597d0dd0cdaaf3cac8ade4c94d6c0141f6d
Niclas Borlin <niclas.borlin@cs.umu.se>
2019-11-14 22:57:23 +0100

    Add parsing of DBAT XML operation set_initial_values

--- code/script/private/parsesetinitialiovalues.m
+++ code/script/private/parsesetinitialiovalues.m
@@ -13,15 +13,15 @@ function s=parsesetinitialiovalues(s,xml)
 %       pp     - comma-separated (x,y) with principal point in
 %                camera units. The special string 'default' will 
 %                set pp as the center of the sensor. 
+%       aspect - scalar with pixel aspect ratio. The string 'default'
+%                sets the aspect ratio to unity.
+%       skew   - scalar with skew. The string 'default' sets a zero value.
 %       K      - nK-vector with radial lens distortion
 %                coefficients. The string 'default' corresponds to
 %                all zeros.
 %       P      - nP-vector with radial lens distortion
 %                coefficients. The string 'default' corresponds to
 %                all zeros.
-%       aspect - scalar with pixel aspect ratio. The string 'default'
-%                sets the aspect ratio to unity.
-%       skew   - scalar with skew. The string 'default' sets a zero value.
 %       all    - If equal to the string 'loaded', uses the
 %                pre-loaded values. If equal to 'default', the
 %                default values indicated above are set.
@@ -50,10 +50,120 @@ end
 % Parse each subblock
 fn=fieldnames(xml);
 for i=1:length(fn)
+    % Every field should have a Text subfield and nothing else
+    sub=xml.(fn{i});
+    [ok,msg]=checkxmlfields(sub,'Text');
+    if ~ok, error('DBAT XML script set_initial_values/IO error: %s',msg); end
+    
     switch fn{i}
       case 'all'
-        % Copy all loaded prior IO values.
-        s.IO.val=s.prior.IO.val;
+        switch sub.Text
+          case 'loaded'
+            % Copy all loaded prior IO values.
+            s=setcamvals(s,'loaded');
+          case 'default'
+            % Set all values as default.
+            s=setcamvals(s,'default',s.IO.cam.focal);
+          otherwise
+            error(['DBAT XML set initial values/IO error: Unknown ' ...
+                   '''all'' string ''%s'''],sub.Text);
+        end
+      case 'cc'
+        switch sub.Text
+          case {'focal','default'}
+            s=setcamvals(s,'cc',s.IO.cam.focal);
+          case 'loaded'
+            % Copy prior cc values.
+            s=setcamvals(s,'cc',getcamvals(s,'prior','cc'));
+          otherwise
+            cc=sscanf(sub.Text,'%f');
+            if isscalar(cc)
+                s=setcamvals(s,'cc',cc);
+            else
+                error(['DBAT XML set initial values/IO/cc error: Bad ' ...
+                       'cc value: %s'],sub.Text);
+            end
+        end
+      case 'pp'
+        switch sub.Text
+          case 'default'
+            s=setcamvals(s,'pp',0.5*diag([1,-1])*s.IO.sensor.ssSize);
+          case 'loaded'
+            % Copy prior pp values.
+            s=setcamvals(s,'pp',getcamvals(s,'prior','pp'));
+          otherwise
+            pp=sscanf(sub.Text,'%f,');
+            if length(pp)==2
+                s=setcamvals(s,'pp',repmat(pp,1,size(s.IO.val,2)));
+            else
+                error(['DBAT XML set initial values/IO/pp error: Bad ' ...
+                       'pp values: %s'],sub.Text);
+            end
+        end
+      case 'aspect'
+        switch sub.Text
+          case 'default'
+            s=setcamvals(s,'as',0);
+          case 'loaded'
+            % Copy prior aspect value.
+            s=setcamvals(s,'as',getcamvals(s,'prior','as'));
+          otherwise
+            aspect=sscanf(sub.Text,'%f');
+            if isscalar(aspect)
+                s=setcamvals(s,'as',1-aspect);
+            else
+                error(['DBAT XML set initial values/IO/aspect error: Bad ' ...
+                       'aspect value: %s'],sub.Text);
+            end
+        end
+      case 'skew'
+        switch sub.Text
+          case 'default'
+            s=setcamvals(s,'sk',0);
+          case 'loaded'
+            % Copy prior skew value.
+            s=setcamvals(s,'sk',getcamvals(s,'prior','sk'));
+          otherwise
+            skew=sscanf(sub.Text,'%f');
+            if isscalar(skew)
+                s=setcamvals(s,'sk',skew);
+            else
+                error(['DBAT XML set initial values/IO/skew error: Bad ' ...
+                       'skew value: %s'],sub.Text);
+            end
+        end
+      case 'K'
+        switch sub.Text
+          case 'default'
+            s=setcamvals(s,'K',0);
+          case 'loaded'
+            % Copy prior K values.
+            s=setcamvals(s,'K',getcamvals(s,'prior','K'));
+          otherwise
+            K=sscanf(sub.Text,'%f,');
+            if length(K)==s.IO.model.nK
+                s=setcamvals(s,'K',repmat(K,1,size(s.IO.val,2)));
+            else
+                error(['DBAT XML set initial values/IO/K error: Bad ' ...
+                       'K values: %s'],sub.Text);
+            end
+        end
+      case 'P'
+        switch sub.Text
+          case 'default'
+            s=setcamvals(s,'P',0);
+          case 'loaded'
+            % Copy prior P values.
+            s=setcamvals(s,'P',getcamvals(s,'prior','P'));
+          otherwise
+            P=sscanf(sub.Text,'%f,');
+            if length(P)==s.IO.model.nP
+                s=setcamvals(s,'P',repmat(P,1,size(s.IO.val,2)));
+            else
+                error(['DBAT XML set initial values/IO/P error: Bad ' ...
+                       'P values: %s'],sub.Text);
+            end
+        end
       otherwise
         error('DBAT XML set initial values/IO error: Unknown field ''%s''',...
               fn{i});
