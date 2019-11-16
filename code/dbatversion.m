ea5c85f88e11fcc033488decd15ef7d925215933
Niclas Borlin <niclas.borlin@cs.umu.se>
2019-11-16 14:21:16 +0100

    Bump version number

--- code/dbatversion.m
+++ code/dbatversion.m
@@ -12,9 +12,9 @@ function [v,date]=dbatversion(gitToo)
 if nargin<1, gitToo=false; end
 
 % Should always be x.y.z or x.y.z.w.
-v='0.8.5.1';
+v='0.9.0.0';
 
-date='2019-01-13';
+date='2019-11-16';
 
 if gitToo
     g=gitversion;
