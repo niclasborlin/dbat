1d42a6c54d214d8bbcd02bf9e31ed12866d11a7a
Niclas Borlin <niclas.borlin@cs.umu.se>
2019-11-22 12:53:40 +0100

    Strip whitespace from path names

--- code/script/private/parsepath.m
+++ code/script/private/parsepath.m
@@ -4,8 +4,14 @@ function p=parsepath(p,baseDir)
 %   P=PARSEPATH(P,BASEDIR) prepends BASEDIR to P is P is a relative
 %   path, otherwise returns P unchanged. P is an absolute path if it
 %   starts with slash, backslash, or 'X:', where X is any letter.
+%
+%   Any leading and trailing whitespace is stripped from P before
+%   processing.
 
 narginchk(2,2)
+
+% Remove all leading and trailing whitespace.
+p=strip(p);
 
 if isempty(p) || p(1)=='/' || p(1)=='\' || ...
         (length(p)>=2 && isstrprop(p(1),'alpha') && p(2)==':')
