ac0b874a86dfeb074a3add986bdc4439a754dfcc
Niclas Borlin <niclas.borlin@cs.umu.se>
2019-11-28 11:09:36 +0100

    Add support for EO output file

--- code/script/testrundbatscript.m
+++ code/script/testrundbatscript.m
@@ -1,5 +1,14 @@
-camcaldir=fullfile(fileparts(dbatroot),'/data/script/camcaldemo');
+switch 1
+  case 1
+    srcDir=fullfile(fileparts(dbatroot),'/data/script/camcaldemo');
 
-f=fullfile(camcaldir,'camcaldemo.xml');
+    f=fullfile(srcDir,'camcaldemo.xml');
+  case 2
+    srcDir=fullfile(fileparts(dbatroot),'/data/script/romabundledemo');
+
+    f=fullfile(srcDir,'romabundledemo.xml');
+  otherwise
+    error('Bad test case');
+end
 
 [s,xml]=rundbatscript(f,true);
