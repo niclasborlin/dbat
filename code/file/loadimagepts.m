e0c616ecb9997d868db4c5d057aff8aa688ccb30
Niclas Borlin <niclas.borlin@cs.umu.se>
2019-12-27 01:57:26 +0100

    Speed up loading of image points with fscanf

--- code/file/loadimagepts.m
+++ code/file/loadimagepts.m
@@ -1,4 +1,4 @@
-function pts=loadimagepts(fName,fmt,sep,cmt)
+function pts=loadimagepts2(fName,fmt,sep,cmt)
 %LOADIMAGEPTS Load image points from text file.
 %
 %   PTS=LOADIMAGEPTS(FNAME,FORMAT) loads 2D image point information
@@ -67,75 +67,155 @@ if ~isempty(setdiff(fmtParts,knownParts))
     error('%s: Invalid format parts: %s',mfilename,bad{1});
 end
 
-id=zeros(1,0);
-im=zeros(1,0);
-pos=zeros(2,0);
-std=zeros(2,0);
+% Convert parts to scanf strings
+idIx=0;
+imIx=0;
+posIx=[0,0];
+stdIx=[0,0];
+fieldsToRead=0;
 
-lineNo=0;
+scanfStrings=cell(size(fmtParts));
+for i=1:length(fmtParts)
+    switch fmtParts{i}
+      case 'id'
+        scanfStrings{i}='%u';
+        idIx=i;
+        fieldsToRead=fieldsToRead+1;
+      case 'im'
+        scanfStrings{i}='%u';
+        imIx=i;
+        fieldsToRead=fieldsToRead+1;
+      case 'ignored'
+        scanfStrings{i}='%*f';
+      case 'x'
+        scanfStrings{i}='%g';
+        posIx(1)=i;
+        fieldsToRead=fieldsToRead+1;
+      case 'y'
+        scanfStrings{i}='%g';
+        posIx(2)=i;
+        fieldsToRead=fieldsToRead+1;
+      case 'sx'
+        scanfStrings{i}='%g';
+        stdIx(1)=i;
+        fieldsToRead=fieldsToRead+1;
+      case 'sy'
+        scanfStrings{i}='%g';
+        stdIx(2)=i;
+        fieldsToRead=fieldsToRead+1;
+      case 'sxy'
+        scanfStrings{i}='%g';
+        stdIx(1:2)=i;
+        fieldsToRead=fieldsToRead+1;
+    end        
+end
+scanfString=join(scanfStrings,sep);
+scanfString=scanfString{1};
 
 % Initialize progress bar.
 h=waitbar(0,'Loading image points');
 
+% Initialize file pointer.
+pos=ftell(fid);
+
+% Read initial part that may contain comments.
 while ~feof(fid)
     % Read one line and clean from whitespace.
+    pos=ftell(fid);
     s=strip(fgets(fid));
-    lineNo=lineNo+1;
     % Skip blank or line starting with #.
-    if isempty(s) || s(1)==cmt
-        continue;
+    if ~isempty(s) && s(1)~=cmt
+        break;
     end
-    % Split line into parts
-    parts=strip(strsplit(s,sep));
-    
-    if length(parts)~=length(fmtParts)
-        error(['%s: %s, line %d: Wrong number of elements ',...
-               '(got %d, expected %d)'], mfilename, fName, lineNo, ...
-              length(parts), length(fmtParts));
-    end
-
-    if rem(lineNo+1,1000)==0
-        if ~ishandle(h)
-            % Waitbar closed, abort.
-            fclose(fid);
-            err='Aborted by user';
-            if nargout<2, error(err); else return; end
-        else
-            % Update progressbar every 1000 input lines.
-            waitbar(ftell(fid)/sz,h);
-        end
-    end
-    
-    ii=nan;
-    mm=nan;
-    p=nan(2,1);
-    s=zeros(2,1);
-    for i=1:length(fmtParts)
-        switch fmtParts{i}
-          case 'id'
-            ii=sscanf(parts{i},'%d');
-          case 'im'
-            mm=sscanf(parts{i},'%d');
-          case {'x', 'y'}
-            p(abs(fmtParts{i})-abs('x')+1)=sscanf(parts{i},'%f');
-          case {'sx', 'sy'}
-            s(abs(fmtParts{i}(2))-abs('x')+1)=sscanf(parts{i},'%f');
-          case 'sxy'
-            s(:)=sscanf(parts{i},'%f');
-          case 'ignored'
-            % Do nothing
-          otherwise
-            % Should never happen
-            error('%s: Invalid format part: ''%s''',mfilename,fmtParts{i});
-        end
-    end
-    id(end+1)=ii;
-    im(end+1)=mm;
-    pos(:,end+1)=p;
-    std(:,end+1)=s;
 end
 
-if ishandle(h), waitbar(ftell(fid)/sz,h); end
+if ~ishandle(h)
+    % Waitbar closed, abort.
+    fclose(fid);
+    err='Aborted by user';
+    if nargout<2, error(err); else return; end
+else
+    % Update progressbar.
+    waitbar(ftell(fid)/sz,h);
+end
+
+% Seek back to start of first data line.
+fseek(fid,pos,'bof');
+
+% Read 10 lines to estimate bytes/line.
+for i=1:10
+    fgetl(fid);
+end
+
+bytesPerLine=(ftell(fid)-pos)/10;
+
+% Seek back to start of first data line.
+fseek(fid,pos,'bof');
+
+% Approximate number of data lines.
+numDataLines=ceil((sz-pos)/bytesPerLine);
+
+% Pre-allocate output data.
+id=nan(1,numDataLines);
+im=nan(1,numDataLines);
+pos=nan(2,numDataLines);
+std=zeros(2,numDataLines);
+
+% Last occupied data element.
+i=0;
+
+% Read data blocks from file
+blockSize=100;
+while ~feof(fid)
+    [A,cnt]=fscanf(fid,scanfString,[fieldsToRead,blockSize]);
+    rows=floor(cnt/fieldsToRead);
+    if rem(cnt,fieldsToRead)~=0
+        error('Bad data after reading %d rows + %d elements in file %s',...
+              rows,rem(cnt,fieldToRead),fName);
+    end
+    if ~ishandle(h)
+        % Waitbar closed, abort.
+        fclose(fid);
+        err='Aborted by user';
+        if nargout<2, error(err); else return; end
+    else
+        % Update progressbar.
+        waitbar(ftell(fid)/sz,h);
+    end
+
+    if idIx~=0
+        id(i+(1:rows))=A(idIx,:);
+    end
+    if imIx~=0
+        im(i+(1:rows))=A(imIx,:);
+    end
+    if all(posIx)~=0
+        pos(:,i+(1:rows))=A(posIx,:);
+    elseif posIx(1)~=0
+        pos(1,i+(1:rows))=A(posIx(1),:);
+    elseif posIx(2)~=0
+        pos(2,i+(1:rows))=A(posIx(2),:);
+    end
+    if all(stdIx)~=0
+        std(:,i+(1:rows))=A(stdIx,:);
+    elseif stdIx(1)~=0
+        std(1,i+(1:rows))=A(stdIx(1),:);
+    elseif stdIx(2)~=0
+        std(2,i+(1:rows))=A(stdIx(2),:);
+    end
+    
+    i=i+rows;
+end
+
+if ishandle(h)
+    % Update progressbar.
+    waitbar(ftell(fid)/sz,h);
+end
+
+id=id(1:i);
+im=im(1:i);
+pos=pos(:,1:i);
+std=std(:,1:i);
 
 fclose(fid);
 
